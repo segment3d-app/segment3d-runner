0d458b4caa8341b7b36909963776fe095455140f
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-05-26 23:52:55 +0700

    feat: add download pretrained sam

--- setups/saga.sh
+++ setups/saga.sh
@@ -4,6 +4,10 @@ source ~/.bashrc
 set -e
 cd ../models/saga
 
-echo "saga: [1/1] initializing environment..."
+echo "saga: [1/2] initializing environment..."
 
 conda env create -f environment.yml
+
+echo "saga: [2/2] downloading pre-trained SAM model..."
+
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -O sam.pth
