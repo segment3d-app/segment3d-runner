4ce6a6d3e8c2e1169e14d6b44550ff3d98a7d91f
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-06-09 15:39:48 +0700

    fix: update optional arguments for options

--- src/models.py
+++ src/models.py
@@ -20,12 +20,13 @@ class Model:
         self.conda_env = conda_env
         self.model_path = model_path
 
-    def run_command(self, command: str, env: Dict[str, str]):
+    def run_command(self, command: str, env: Dict[str, str] = None):
         env = os.environ.copy()
         env["CUDA_VISIBLE_DEVICES"] = ",".join(pick_available_gpus())
 
-        for key, value in env.items():
-            env[key] = value
+        if env is not None:
+            for key, value in env.items():
+                env[key] = value
 
         command = self.__append_environment(parse_command(command))
         process = subprocess.run(
