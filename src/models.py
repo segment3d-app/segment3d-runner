836337233140dcccaeeefa727bcc78fdd59ba017
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-06-12 09:51:14 +0700

    feat: update saga segment

--- src/models.py
+++ src/models.py
@@ -219,6 +219,14 @@ class SagaTrainFeaturesError(Exception):
     pass
 
 
+class SagaSegmentError(Exception):
+    pass
+
+
+class SagaRenderError(Exception):
+    pass
+
+
 class Saga(Model):
     def __init__(self, asset_id: str, asset_type: str):
         Model.__init__(
@@ -242,6 +250,16 @@ class Saga(Model):
 
     async def train_features(self):
         await asyncio.get_event_loop().run_in_executor(None, self.__train_features)
+
+    async def segment(
+        self, segment_id: str, image_index: int, mask_index: int, x: int, y: int
+    ):
+        await asyncio.get_event_loop().run_in_executor(
+            None, self.__segment, segment_id, image_index, mask_index, x, y
+        )
+
+    async def render(self, segment_id: str):
+        await asyncio.get_event_loop().run_in_executor(None, self.__render, segment_id)
 
     def __extract_features(self):
         command = f"""python {os.path.join(self.model_path, "extract_features.py")}
@@ -283,3 +301,38 @@ class Saga(Model):
         process = self.run_command(command)
         if process.returncode != 0:
             raise SagaTrainFeaturesError(process.stderr)
+
+    def __segment(
+        self, segment_id: str, image_index: int, mask_index: int, x: int, y: int
+    ):
+        command = f"""python {os.path.join(self.model_path, "prompt_segmenting.py")}
+            --sam_checkpoint_path {os.path.join(self.model_path, "sam.pth")}
+            --image_root {self.asset_path}
+            --mask_idx {mask_index}
+            --image_idx {image_index}
+            --object "{segment_id}"
+            --target_coord "[[{x}, {y}]]"
+            --model_path {os.path.join(self.asset_path, "saga")}
+            --iterations 7000
+        """
+
+# python models/saga/prompt_segmenting.py --sam_checkpoint_path models/saga/sam.pth --image_root assets/403a4bbc-c7da-4546-a7a2-f7a29f22a012 --mask_idx 2 --image_idx 1 --object "aaa" --target_coord "[[300, 300]]" --iterations 7000 --model_path assets/403a4bbc-c7da-4546-a7a2-f7a29f22a012/saga
+
+        process = self.run_command(command)
+        if process.returncode != 0:
+            raise SagaSegmentError(process.stderr)
+
+    def __render(self, segment_id: str):
+        command = f"""python {os.path.join(self.model_path, "render.py")}
+            --m {os.path.join(self.asset_path, "saga")}
+            --precomputed_mask {os.path.join(self.asset_path, "saga/segmentation", segment_id, "final_mask.pt")}
+            --target scene
+            --segment
+            --iterations 7000
+        """
+
+# python models/saga/render.py --m assets/403a4bbc-c7da-4546-a7a2-f7a29f22a012/saga --precomputed_mask assets/403a4bbc-c7da-4546-a7a2-f7a29f22a012/saga/segmentation/aaa/final_mask.pt --target scene --segment
+
+        process = self.run_command(command)
+        if process.returncode != 0:
+            raise SagaRenderError(process.stderr)
